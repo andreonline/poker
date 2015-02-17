a07c3d2401bce8be914436d69333a0360bb49ef5
mixplick <mixplick87@gmail.com>
2015-02-17 13:17:13 -0300

    commit [17/02/2015 13:17][HOME]

--- view/form_game.php
+++ view/form_game.php
@@ -1,94 +1,115 @@
-<script type="text/javascript">
-    /*validação*/
-    $(document).ready(function () {
+<?php
+$valores = array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+?>
 
-        $('#cadastrar').validate({
-            rules: {
-                nome: {required: true},
-                email: {required: true, email: true},
-                senha: {required: true, rangelength: [6, 12]},
-                cel: {required: true},
-                team: {required: true},
-                login: {required: true}
-            },
-            messages: {
-                nome: {required: ''},
-                email: {required: '', email: "Digite um e-mail válido"},
-                senha: {required: '', rangelength: "Entre 6 e 12 caracteres"},
-                cel: {required: ''},
-                team: {required: ''},
-                login: {required: ''}
-            },
-            submitHandler: function (form) {
-                var dados = $(form).serialize();
-                $.ajax({
-                    type: "POST",
-                    url: "processa_game.php",
-                    data: dados,
-                    success: function (dados) {
-                        if (dados == "I") {
-                            alert('Cadastro efetuado com sucesso!');
-                        } else {
-                            $("#contentee").html('');
-                            $("#contentee").append(dados);
-                        }
-                    },
-                    beforeSend: function () {
-                        $('.loading').fadeIn('fast');
-                    },
-                    complete: function () {
-                        $('.loading').fadeOut('fast');
-                    }
-
-                });
-
-                return false;
-            }
-        });
-    });
-</script>
-
-<div class="row" style="max-width: 1024px; min-height: 482px; margin-top: 8%">
-    <form id="cadastrar" class="col s12" name="cadastrar" action="processa_user.php" method="post">
+<div class="row" style="max-width: 1024px; margin-top: 8%">
+    <form id="cadastrar" class="col s12" name="cadastrar" action="processa_game.php" method="post">
         <input type="hidden" id="action" name="action" />
+
         <div class="row">
-            <div class="input-field col s6">
-                <i class="mdi-social-person prefix"></i>
-                <input id="nome" name="nome" type="text" class="validate" required>
-                <label for="nome">Nome</label>
-            </div>
-            <div class="input-field col s6">
-                <i class="mdi-social-people prefix"></i>
-                <input id="team" name="team" type="text" class="validate" required>
-                <label for="team">Team</label>
-            </div>
-        </div>
-        <div class="row">
-            <div class="input-field col s6">
-                <i class="mdi-action-settings-power prefix"></i>
-                <input id="login" name="login" type="text" class="validate" required>
-                <label for="login">Login</label>
+            <div class="col s6">
+                <input class="with-gap" name="tipo" type="radio" id="cash" />
+                <label for="cash">Cash</label>
             </div>
 
-            <div class="input-field col s6">
-                <i class="mdi-communication-vpn-key prefix"></i>
-                <input id="senha" name="senha" type="password" class="validate" required>
-                <label for="senha">Senha</label>
+            <div class="col s6">
+                <input class="with-gap" name="tipo" type="radio" id="torneio" />
+                <label for="torneio">Torneio</label>
             </div>
         </div>
+
         <div class="row">
             <div class="input-field col s6">
-                <i class="mdi-content-markunread prefix"></i>
-                <input id="email" name="email" type="email" class="validate" required>
-                <label for="email">E-mail</label>
+                <i class="mdi-action-home prefix"></i>
+                <input id="team" name="local" type="text" class="validate" required>
+                <label for="local">Local</label>
             </div>
 
-            <div class="input-field col s6">
-                <i class="mdi-communication-phone prefix"></i>
-                <input id="cel" name="cel" type="tel" class="validate" required>
-                <label for="cel">Celular</label>
+            <div class="col s6">
+                <label>Buy-In</label>
+                <select class='browser-default'>
+                    <option value='' disabled selected>Escolha o Buy-In</option>
+
+                    <?php
+                    foreach ($valores as $value) {
+                        echo "<option  value='$value'>R$ $value</option> ";
+                    }
+                    ?>
+                </select>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col s6">
+                <label>Re-Buy</label>
+                <select class='browser-default'>
+                    <option value='' disabled selected>Escolha o Re-Buy</option>
+
+                    <?php
+                    foreach ($valores as $value) {
+                        echo "<option  value='$value'>R$ $value</option> ";
+                    }
+                    ?>
+                </select>
+            </div>
+
+
+            <div class="col s6">
+
+                <label>Add-On</label>
+                <select class='browser-default'>
+                    <option value='' disabled selected>Escolha o Add-On</option>
+
+                    <?php
+                    foreach ($valores as $value) {
+                        echo "<option  value='$value'>R$ $value</option> ";
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s3" style="margin: 3%">
+                <input type="checkbox" id="player" />
+                <label for="player">Mixplick</label>
+            </div>
+
+
+            <div class="col s3">
+                <label>Re-Buy</label>
+                <select class='browser-default'>
+                    <option value='' disabled selected>Quantidade</option>
+
+                    <?php
+                    for ($index = 0; $index < 21; $index++) {
+                        echo "<option  value='$index'> $index </option> ";
+                    }
+                    ?>
+                </select>
+            </div>
+
+            <div class="col s3" style="margin: 0.25%">
+                <label class="left">Add-On</label>
+                <div style="clear: both"></div>
+                <div style="margin: 5%"></div>
+                <div class="col s6">
+                    <input class="with-gap" name="addon" type="radio" id="sim" />
+                    <label for="sim">Sim</label>
+                </div>
+
+                <div class="col s6">
+                    <input class="with-gap" name="addon" type="radio" id="nao" />
+                    <label for="nao">Não</label>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
         <button class="btn waves-effect waves-light right" type="submit">Enviar
             <i class="mdi-content-send right"></i>
         </button>
